880a1b0dd111fc7b9171357ed48ad4ec09ee938a
joefstack <joefstack@gmail.com>
2021-01-19 23:10:54 -0700

    added app reducer and app context

---
+++ src/helpers/RecipeContext.js
@@ -0,0 +1,58 @@
+import React, { createContext, useReducer, useEffect } from "react";
+import RecipeReducer from "./RecipeReducer";
+
+// declare initial state from localstorage
+const initialState = {
+  makeList: localStorage.getItem("makeList")
+    ? JSON.parse(localStorage.getItem("makeList"))
+    : [],
+  finishedList: localStorage.getItem("finishedList")
+    ? JSON.parse(localStorage.getItem("finishedList"))
+    : [],
+};
+
+// create global app context
+const RecipeContext = createContext(initialState);
+
+// export provider components
+const RecipeProvider = (props) => {
+  const [state, dispatch] = useReducer(RecipeReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem("makeList", JSON.stringify(state.makeList));
+    localStorage.setItem("finishedList", JSON.stringify(state.finishedList));
+  }, [state]);
+
+  // app reducer actions
+  const addRecipeToMakeList = (recipe) => {
+    dispatch({ type: "ADD_RECIPE_TO_MAKE_LIST", payload: recipe });
+  };
+  const removeRecipeFromMakeList = (id) => {
+    dispatch({ type: "REMOVE_RECIPE_FROM_MAKE_LIST", payload: id });
+  };
+  const addRecipeToFinishedList = (recipe) => {
+    dispatch({ type: "ADD_RECIPE_TO_FINISHED_LIST", payload: recipe });
+  };
+  const moveToMakeList = (recipe) => {
+    dispatch({ type: "MOVE_TO_MAKE_LIST", payload: recipe });
+  };
+  const removeFromFinishedList = (id) => {
+    dispatch({ type: "REMOVE_FROM_FINISHED_LIST", payload: id });
+  };
+
+  return (
+    <RecipeContext.Provider
+      value={{
+        makeList: state.makeList,
+        finishedList: state.finishedList,
+        addRecipeToMakeList,
+        removeRecipeFromMakeList,
+        addRecipeToFinishedList,
+        moveToMakeList,
+        removeFromFinishedList,
+      }}
+    >
+      {props.children}
+    </RecipeContext.Provider>
+  );
+};
---
+++ src/helpers/RecipeReducer.js
@@ -0,0 +1,39 @@
+export default (state, action) => {
+  switch (action.type) {
+    case "ADD_RECIPE_TO_MAKE_LIST":
+      return {
+        ...state,
+        makeList: [action.payload, ...state.makeList],
+      };
+    case "REMOVE_RECIPE_FROM_MAKE_LIST":
+      return {
+        ...state,
+        makeList: state.makeList.filter(
+          (recipe) => recipe.id !== action.payload
+        ),
+      };
+    case "ADD_RECIPE_TO_FINISHED_LIST":
+      return {
+        ...state,
+        makeList: state.makeList.filter(
+          (recipe) => recipe.id !== action.payload.id
+        ),
+        finishedList: [action.payload, ...state.finishedList],
+      };
+    case "MOVE_TO_MAKE_LIST":
+      return {
+        ...state,
+        finishedList: state.finishedList.filter(
+          (recipe) => recipe.id !== action.payload.id
+        ),
+        makeList: [action.payload, ...state.makeList],
+      };
+    case "REMOVE_FROM_FINISHED_LIST":
+      return {
+        ...state,
+        finishedList: state.finishedList.filter((recipe) => recipe.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
